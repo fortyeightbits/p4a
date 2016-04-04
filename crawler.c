91f9d1a3b2e1284031f203f64516c8ac2d687a49
Sengming <nitricrage@gmail.com>
2016-04-04 08:48:05 +0800

    Just moved function definitions out of crawler.

--- crawler.c
+++ crawler.c
@@ -20,30 +20,30 @@ int crawl(char *start_url,
 	pthread_cond_t linkQueueFill = PTHREAD_COND_INITIALIZER;
 	Queue_t linkQueue;
 	Queue_init(&linkQueue);
-
-	//downloader is the consumer of links
-	void* downloader(void *arg) {
-		Pthread_mutex_lock(&linkQueueMutex);
-		while (linkQueue->size == 0) //sleep if there's no links to get
-			Pthread_cond_wait(&linkQueueFill, &linkQueueMutex);
-		downloadpage(); //TODO download function
-		Pthread_cond_signal(&linkQueueEmpty);
-		Pthread_mutex_unlock(&linkQueueMutex);
-7 	}
-
-	//parser is the producer of links
-	void* parser(void *arg) {
-		Pthread_mutex_lock(&linkQueueMutex);
-		while (linkQueue->size == queue_size) //sleep if queue is full. Is this right?
-			Pthread_cond_wait(&linkQueueEmpty, &linkQueueMutex);
-		parseparge(linkQueue, start_url, (*_fetch_fn)(char *url)); //TODO: parse page
-		Pthread_cond_signal(&linkQueueFill);
-		Pthread_mutex_unlock(&linkQueueMutex);
-	}
 	
 	free(page);
 
   return -1;
+}
+
+//downloader is the consumer of links
+void* downloader(void *arg) {
+    Pthread_mutex_lock(&linkQueueMutex);
+    while (linkQueue->size == 0) //sleep if there's no links to get
+        Pthread_cond_wait(&linkQueueFill, &linkQueueMutex);
+    downloadpage(); //TODO download function
+    Pthread_cond_signal(&linkQueueEmpty);
+    Pthread_mutex_unlock(&linkQueueMutex);
+}
+
+//parser is the producer of links
+void* parser(void *arg) {
+    Pthread_mutex_lock(&linkQueueMutex);
+    while (linkQueue->size == queue_size) //sleep if queue is full. Is this right?
+        Pthread_cond_wait(&linkQueueEmpty, &linkQueueMutex);
+    parseparge(linkQueue, start_url, (*_fetch_fn)(char *url)); //TODO: parse page
+    Pthread_cond_signal(&linkQueueFill);
+    Pthread_mutex_unlock(&linkQueueMutex);
 }
 
 //BUGGLE!!! Not sure if I'm using callbacks/functionptr(?) right. :( Trying to pass _fetch_fn to this parsepage function.
