0b60f71092a84f8a09b75c54ba2202ccc7db6f7a
Joanne Lee <joanne@galapagos-10.cs.wisc.edu>
2016-04-06 23:41:48 -0500

    nyam nyam got it to work yay

--- queuetest.c
+++ queuetest.c
@@ -44,6 +44,7 @@ typedef struct DeQArgPacket{
 
 
 void Queue_init(Queue_t* q){
+	printf("%d: init getting lock\n", pthread_self());
 	pthread_mutex_lock(&linkQueueMutex);
 	//////////
 	q->size = 0;
@@ -53,15 +54,20 @@ void Queue_init(Queue_t* q){
 	q->tail = temp;	
 	//////////
 	pthread_mutex_unlock(&linkQueueMutex);
+	printf("%d: init released lock\n", pthread_self());
 }
 
 int Queue_enqueue(char* x, Queue_t* q, int queue_size) {
 	
+	printf("%d: enqueue getting lock\n", pthread_self());
 	pthread_mutex_lock(&linkQueueMutex);
-	while (q->size == queue_size) //TODO: get queue size
-        pthread_cond_wait(&linkQueueEmpty, &linkQueueMutex);
+	printf("%d: eequeue got lock\n", pthread_self());
+	while (q->size == queue_size){ //TODO: get queue size
+        printf("%d: enqueue sleeping because queue is full\n", pthread_self());
+		pthread_cond_wait(&linkQueueEmpty, &linkQueueMutex);
+	}
 	//////////////////////////
-	
+	printf("%d: enqueue either woke up or never slept\n", pthread_self());
 	if(q->head->data == NULL)
 	{
 		q->tail->data = x;		
@@ -76,19 +82,25 @@ int Queue_enqueue(char* x, Queue_t* q, int queue_size) {
 	}
 	q->size++;
 	//////////////////////////
+	printf("%d: enqueue signalling\n", pthread_self());
 	pthread_cond_signal(&linkQueueFill);
+	printf("%d: enqueue going to release lock\n", pthread_self());
 	pthread_mutex_unlock(&linkQueueMutex);
+	printf("%d: enqueue released lock\n", pthread_self());
 	return 0;
 }
 
 //value has head->next's value, not the head
 int Queue_dequeue(Queue_t *q, char** returnvalue) {
-	
+	printf("%d: dequeue getting lock\n", pthread_self());
 	pthread_mutex_lock(&linkQueueMutex);
-	while (q->size == 0) //sleep if there's no links to get
-        pthread_cond_wait(&linkQueueFill, &linkQueueMutex);
+	printf("%d: dequeue got lock\n", pthread_self());
+	while (q->size == 0){ //sleep if there's no links to get
+        printf("%d: dequeue sleeping\n", pthread_self());
+		pthread_cond_wait(&linkQueueFill, &linkQueueMutex);
+	}
 	/////////////////
-	
+	printf("%d: dequeue woke up\n", pthread_self());
 	Node_t *tmp = q->head;
 	*returnvalue = q->head->data;
 	Node_t *newHead = tmp->next;
@@ -99,15 +111,22 @@ int Queue_dequeue(Queue_t *q, char** returnvalue) {
 		temp->next = NULL;
 		q->head = temp;
 		q->tail = temp;	
+		
+		printf("%d: dequeue signalling(-1)\n", pthread_self());
+		pthread_cond_signal(&linkQueueEmpty);
+		printf("%d: dequeue going to release lock\n", pthread_self());
+		pthread_mutex_unlock(&linkQueueMutex);
+		printf("%d: dequeue release lock (-1)\n", pthread_self());
 		return -1; // queue was empty
 	}
   	q->head = newHead;
 	free(tmp);
 	q->size--;
 	/////////////////////////
+	printf("%d: dequeue signalling\n", pthread_self());
 	pthread_cond_signal(&linkQueueEmpty);
 	pthread_mutex_unlock(&linkQueueMutex);
-	
+	printf("%d: dequeue release lock\n", pthread_self());
 	return 0;
 }
 
@@ -116,20 +135,30 @@ int Queue_dequeue(Queue_t *q, char** returnvalue) {
 void *enq_helper(void* argPtr)
 {
 	Queue_enqueue(((EPacket_t*)(argPtr))->x, ((EPacket_t*)(argPtr))->q, ((EPacket_t*)(argPtr))->queue_size);
+	printf("%d: enqueuehelper getting mainmutex\n", pthread_self());
 	pthread_mutex_lock(&mainMutex);
+	printf("%d: enqueuehelper got mainmutex, ++work \n", pthread_self());
 	workInSystem++;
+	printf("%d: enqueuehelper going to release mainmutex\n", pthread_self());
 	pthread_mutex_unlock(&mainMutex);
+	printf("%d: enqueuehelper released mainmutex\n", pthread_self());
 }
 
 //downloader dequeues 
 void *deq_helper(void* argPtr)
 {
 	Queue_dequeue(((DPacket_t*)argPtr)->q,((DPacket_t*)argPtr)->returnvalue);
+	printf("%d: dequeuehelper getting mainmutex\n", pthread_self());
 	pthread_mutex_lock(&mainMutex);
+	printf("%d: dequeuehelper got mainmutex, --work \n", pthread_self());
 	workInSystem--;
-	while (workInSystem == 0)
+	if (workInSystem == 0){
+		printf("%d: dequeuehelper waking main up, no more work\n", pthread_self());
 		pthread_cond_signal(&noWorkInSystem);
+	}
+	printf("%d: dequeuehelper wakes, unlocking mainmutex\n", pthread_self());
 	pthread_mutex_unlock(&mainMutex);
+	printf("%d: dequeuehelper released mainmutex\n", pthread_self());
 	//Remember that in the real crawler, workinsystem only incremented when something enqueued to linkqueue, dec when dequeued from pagequeue
 }
 
@@ -175,18 +204,28 @@ int main(int argc, char* argv[])
 		pthread_create(&consumer_pool[k], NULL, deq_helper, ((void*)(&consumerArgs[k])));				
 	}
 	
+	printf("main getting mainmutex lock\n");
 	pthread_mutex_lock(&mainMutex);
-	while (workInSystem != 0 && kiutqueue.size != 0) 
+	printf("main got mainmutex lock\n");
+	
+	while (workInSystem != 0 && kiutqueue.size != 0) {
+		printf("main sleeping while waiting for 0 workinsystem\n");
 		pthread_cond_wait(&noWorkInSystem,&mainMutex); //sleep while workers not done
+	}
+	printf("main woke up or never slept, is unlocking\n");
 	pthread_mutex_unlock(&mainMutex);
 	// Signalling for this condition has yet to be done Buggle, please look into it.	
-
+	printf("main released mainmutex lock\n");
+	
 	int m;
 	for(m = 0; m < PRODUCERS; m++)
 	{
 		pthread_join(producer_pool[m], NULL);
 		pthread_join(consumer_pool[m], NULL);				
 	}
+	
+	printf("done\n");
+	return 0;
 }
 		
 		
